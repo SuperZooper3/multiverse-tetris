5b028bf19259f0e460804cde2c83063d5f25f10a
PopoLleKok0 <Mouadbenlahbibca@gmail.com>
2025-01-18 14:29:20 -0500

    convert tetris controller to class-based structure (works)

--- Scripts/tetris/tetrisControler.js
+++ Scripts/tetris/tetrisControler.js
@@ -1,193 +1,196 @@
- //astetics for the game
- 
- const boxHeight = boxWidth;
- const lineHeight = 1;
+ class tetriscontroler {
+    constructor() {
+        this.boxHeight = boxWidth;
+        this.lineHeight = 1;
+        this.gameRunning = false;
+        this.aiRunning = false;
+        this.gameInterval = null;
+        this.tilesCleared = 0;
+        this.tetrisCanvas = null;
+        this.tetris = null;
 
-function resizeButtons(){
-    let width = $("#tetris").width()/20
-    let height = $("#tetris").height()/10
-    $("#tetrisStart").css("left",  $("#tetrisContainer").width()/24);
-    $("#tetrisAIStart").css("right", $("#tetrisContainer").width()/15);
-    $("#tetrisStart").css("font-size",   Math.round(width) + "px");
-    $("#tetrisAIStart").css("font-size", Math.round(width)+ "px");
-    $("#tetrisStart").css("bottom",   height + "px");
-    $("#tetrisAIStart").css("bottom", height+ "px");
-}
+        this.init();
+    }
 
-window.addEventListener("load", resizeButtons);
-window.addEventListener("resize", resizeButtons);
+    init() {
+        window.addEventListener("load", () => this.setup());
+        window.addEventListener("resize", () => this.resizeButtons());
+    }
 
- $(window).bind("load", function () {
-     
-    var tetrisCanvas = new TetrisCanvas(lineHeight,boxHeight,boxWidth);
-    var tetris = new Tetris();
-    var tilesCleared = 0;
-    tetrisCanvas.draw(tetris.predictLanding(),tetris.getGame(),tilesCleared);
+    resizeButtons() {
+        let width = $("#tetris").width() / 20;
+        let height = $("#tetris").height() / 10;
+        $("#tetrisStart").css("left", $("#tetrisContainer").width() / 24);
+        $("#tetrisAIStart").css("right", $("#tetrisContainer").width() / 15);
+        $("#tetrisStart").css("font-size", Math.round(width) + "px");
+        $("#tetrisAIStart").css("font-size", Math.round(width) + "px");
+        $("#tetrisStart").css("bottom", height + "px");
+        $("#tetrisAIStart").css("bottom", height + "px");
+    }
 
-    $("#tetrisStart").click(function(){
-        startGame();
-    });
+    setup() {
+        this.tetrisCanvas = new TetrisCanvas(this.lineHeight, this.boxHeight, boxWidth);
+        this.tetris = new Tetris();
+        this.tetrisCanvas.draw(this.tetris.predictLanding(), this.tetris.getGame(), this.tilesCleared);
 
-    $("#tetrisAIStart").click(function(){
-        startAI();
-    });
+        $("#tetrisStart").click(() => this.startGame());
+        $("#tetrisAIStart").click(() => this.startAI());
 
-    let gameRunning = false;
-    let aiRunning = false;
-    let gameInterval;
+        document.addEventListener("keydown", (event) => this.keyPress(event));
+    }
 
-    document.addEventListener("keydown",keyPress);       
-
-    function keyPress(event){
-        let keyPressed = event.keyCode;
-        if(keyPressed == 40 || keyPressed == 39 || keyPressed == 38 || keyPressed == 37 || keyPressed == 13 || keyPressed == 32){
+    keyPress(event) {
+        const keyPressed = event.keyCode;
+        if ([40, 39, 38, 37, 13, 32].includes(keyPressed)) {
             event.preventDefault();
         }
-        if(gameRunning || aiRunning){
-            moveTile(keyPressed)
+        if (this.gameRunning || this.aiRunning) {
+            this.moveTile(keyPressed);
         }
     }
 
-    function moveTile(event ){
-        let keyPressed = event
-        switch (keyPressed){
+    moveTile(event) {
+        const keyPressed = event;
+        switch (keyPressed) {
             case 1:
             case 65:
-            case 37:  
-                tetris.moveLeft()
+            case 37:
+                this.tetris.moveLeft();
                 break;
             case 2:
             case 68:
-            case 39: //change to move right
-                // check that all the boxes in the current object can move right
-                tetris.moveRight()
+            case 39:
+                this.tetris.moveRight();
                 break;
             case 4:
             case 83:
-            case 40:  // change to move down
-                tetris.moveDown()
-            break;    
+            case 40:
+                this.tetris.moveDown();
+                break;
             case 5:
             case 65:
             case 32:
-                tetris.swapHold();
+                this.tetris.swapHold();
                 break;
             case 3:
             case 87:
             case 38:
-                tetris.rotate()
+                this.tetris.rotate();
                 break;
             case 6:
             case 13:
-                tetris.dropPiece();
+                this.tetris.dropPiece();
                 break;
             case 7:
-                ai(tetris.getGame());
-                break
+                this.ai(this.tetris.getGame());
+                break;
         }
-        tetrisCanvas.draw(tetris.predictLanding(),tetris.getGame(),tilesCleared);
+        this.tetrisCanvas.draw(this.tetris.predictLanding(), this.tetris.getGame(), this.tilesCleared);
     }
 
-    function startGame(){
-        
-        if(!gameRunning){
-            tilesCleared = 0;
-            setTimeout(function(){ 
-                tetris.reset();
-                moveTile()
-            }, 500);  
-            
-            setTimeout(function(){ 
-                gameInterval = setInterval(function(){ runGame(); }, 800);
-            }, 600);  
-            gameRunning = true;
-            aiRunning = false
-        }else{
-            clearInterval(gameInterval);
-            gameRunning = false;
+    startGame() {
+        if (!this.gameRunning) {
+            this.tilesCleared = 0;
+            setTimeout(() => {
+                this.tetris.reset();
+                this.moveTile();
+            }, 500);
+
+            setTimeout(() => {
+                this.gameInterval = setInterval(() => this.runGame(), 800);
+            }, 600);
+
+            this.gameRunning = true;
+            this.aiRunning = false;
+        } else {
+            clearInterval(this.gameInterval);
+            this.gameRunning = false;
         }
     }
 
-    function startAI(){
-        if(!aiRunning){
-            gameRunning = true
-            startGame();
-            tetris.reset();
-            moveTile()       
-            aiRunning = true;
-            tilesCleared = 0;
-            ai(tetris.getGame());
+    startAI() {
+        if (!this.aiRunning) {
+            this.gameRunning = true;
+            this.startGame();
+            this.tetris.reset();
+            this.moveTile();
+            this.aiRunning = true;
+            this.tilesCleared = 0;
+            this.ai(this.tetris.getGame());
         }
     }
-    function runGame(){
-        moveTile(4);
-        if(!tetris.checkCurrent()){
-            tilesCleared++;
-            if(tetris.createObject(0) === false){
-                gameRunning = false;
-                aiRunning = false;
-                clearInterval(gameInterval);
+
+    runGame() {
+        this.moveTile(4);
+        if (!this.tetris.checkCurrent()) {
+            this.tilesCleared++;
+            if (this.tetris.createObject(0) === false) {
+                this.gameRunning = false;
+                this.aiRunning = false;
+                clearInterval(this.gameInterval);
             }
-            moveTile()
+            this.moveTile();
         }
     }
 
-    function ai(game){   
-        if(game[1] != undefined){        
+    ai(game) {
+        if (game[1] !== undefined) {
             let copyBoard = JSON.parse(JSON.stringify(game[0]));
             let copyCurrentObject = JSON.parse(JSON.stringify(game[1]));
             let copyHoldObject = JSON.parse(JSON.stringify(game[2]));
 
-            for(let i = 0; i < copyCurrentObject.length; i++){
-                copyBoard[copyCurrentObject[i].column + (copyCurrentObject[i].row * 10)].box = undefined;
+            for (let i = 0; i < copyCurrentObject.length; i++) {
+                copyBoard[copyCurrentObject[i].column + copyCurrentObject[i].row * 10].box = undefined;
             }
-            
-            let tetrus = new aiTetrus(copyBoard,copyCurrentObject,copyHoldObject);
+
+            let tetrus = new aiTetrus(copyBoard, copyCurrentObject, copyHoldObject);
             let result = tetrus.placeOneObject();
-            if(result === false){
-                // do gameOver function 
-            }else if(result[1].length > 0){
-                if(result[4]){
-                    tetris.swapHold();              
+            if (result === false) {
+                // game over logic
+            } else if (result[1].length > 0) {
+                if (result[4]) {
+                    this.tetris.swapHold();
                 }
-                takeMoves(result[1]);      
-            }else{ 
-                // do gameOver function
-            }    
-        }    
+                this.takeMoves(result[1]);
+            } else {
+                // game over logic
+            }
+        }
     }
 
-    function takeMoves(moves){     
-        for(let i = moves.length - 1; i > 0; i--){            
-                let action;
-                if(moves[i] == 1){
-                    action = 2;
-                }else if (moves[i] == 2){
-                    action = 1;
-                }else{
-                    action = moves[i];
-                }           
-                setTimeout(function(){ 
-                    moveTile(action);
-                }, (10 * (moves.length - i)));    
+    takeMoves(moves) {
+        for (let i = moves.length - 1; i > 0; i--) {
+            let action;
+            if (moves[i] === 1) {
+                action = 2;
+            } else if (moves[i] === 2) {
+                action = 1;
+            } else {
+                action = moves[i];
+            }
+            setTimeout(() => {
+                this.moveTile(action);
+            }, 10 * (moves.length - i));
         }
 
         let loop = moves[0];
-        for(let i = 0; i < loop; i++){
-                setTimeout(function(){ 
-                    moveTile(4);   
-                }, (10 * (i + moves.length)));     
+        for (let i = 0; i < loop; i++) {
+            setTimeout(() => {
+                this.moveTile(4);
+            }, 10 * (i + moves.length));
         }
-    
-        setTimeout(function(){
-            if(tetris.checkForRow()){
-                tilesCleared++;
-                tetris.createObject(0);
-                if(!gameRunning){
-                    ai(tetris.getGame());
-                } 
+
+        setTimeout(() => {
+            if (this.tetris.checkForRow()) {
+                this.tilesCleared++;
+                this.tetris.createObject(0);
+                if (!this.gameRunning) {
+                    this.ai(this.tetris.getGame());
+                }
             }
-        }, (10 * (loop + moves.length))); 
-                
+        }, 10 * (loop + moves.length));
     }
-});   
+}
+
+// Instantiate the game
+const tetrisGame = new tetriscontroler();
