dbd3666ccc3746c9da950654aacc375f20c4e4f3
roselyn <roselynh100@gmail.com>
2025-01-18 18:16:47 -0500

    remove redundant line from multiverseController

--- Scripts/tetris/multiverseController.js
+++ Scripts/tetris/multiverseController.js
@@ -1,36 +1,31 @@
 class MultiverseController {
-    constructor(numberOfBoards) {
-        this.numberOfBoards = numberOfBoards;
-        this.boards = [];
-    
-        
-        for (let i = 0; i < this.numberOfBoards; i++) {
-            this.boards.push(this.createBoard());
-        }
+  constructor(numberOfBoards) {
+    this.numberOfBoards = numberOfBoards;
+    this.boards = [];
 
-        this.activeBoard = 0;
+    for (let i = 0; i < this.numberOfBoards; i++) {
+      this.boards.push(this.createBoard());
     }
 
-    createBoard() {
-        let id = this.boards.length;
-        let tetrisContainer = $(`
-            <div id="tetrisContainer-${id}" class="thing">
-                <div id="buttonBox-${id}">
-                    <button class="tetrisButton" id="tetrisStart-${id}">Set Active</button>
-                    <button class="tetrisButton" id="tetrisAIStart-${id}">Manual Mode</button>    
-                </div>
-                <canvas id="tetris-${id}"></canvas>
+    this.activeBoard = 0;
+  }
+
+  createBoard() {
+    let id = this.boards.length;
+    let tetrisContainer = $(`
+        <div id="tetrisContainer-${id}" class="thing">
+            <div id="buttonBox-${id}">
+                <button class="tetrisButton" id="tetrisStart-${id}">Set Active</button>
+                <button class="tetrisButton" id="tetrisAIStart-${id}">Manual Mode</button>    
             </div>
-        `);
+            <canvas id="tetris-${id}"></canvas>
+        </div>
+    `);
 
-        $(".gameHolder").append(tetrisContainer);
-        console.log("Created board with id: " + id);
-        if (id === 0) {
-            return new TetrisController(id);
-        }
-        return new TetrisController(id);
-    }
-
+    $(".gameHolder").append(tetrisContainer);
+    console.log("Created board with id: " + id);
+    return new TetrisController(id);
+  }
 }
 
 new MultiverseController(3);
